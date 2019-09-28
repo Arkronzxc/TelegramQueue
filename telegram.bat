339ce6a244368a15473a72227ce7bc00b221d32c
Arkronzxc <ArkronCherf01@gmail.com>
2019-09-28 18:38:14 +0300

    Commit

--- telegram.bat
+++ telegram.bat
@@ -1,14 +1,16 @@
 @echo off
-set /p message=Write commit message
+set /p message=Write commit message 
 
 IF %message%=="" (
  ECHO Empty commit message
  EXIT 1
 )
+
+START cmd.exe /k heroku logs --tail
+
 git add *
 git commit -m %message%
 git push origin master
 git push heroku master
 heroku ps:scale worker=1
 
-START heroku logs --tail
